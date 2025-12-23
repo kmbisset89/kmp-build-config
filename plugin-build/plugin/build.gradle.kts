5ba31ab69b7167a9d796ea62ef97140a7831ed2d
kmbisset89 <42785343+kmbisset89@users.noreply.github.com>
2025-12-23 11:52:10 -0600

    Updated versions

--- plugin-build/plugin/build.gradle.kts
+++ plugin-build/plugin/build.gradle.kts
@@ -22,15 +22,10 @@ dependencies {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_1_8.toString()
-    }
-}
 
 gradlePlugin {
     plugins {
